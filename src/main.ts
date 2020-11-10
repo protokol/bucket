7433f64d595a71d5a773ef3413ad2389eaace03b
Kristjan Kosic <kristjan@protokol.com>
2020-11-10 12:48:49 +0100

    feat: add swagger docs creation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const options = new DocumentBuilder()
+		.setTitle("Protokol Bucket")
+		.setDescription("Decentralized Temporary Storage Engine")
+		.setVersion("1.0")
+		.addTag("peers")
+		.build();
+	const document = SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup("docs", app, document);
+
 	await app.listen(40000);
 }
 bootstrap();
