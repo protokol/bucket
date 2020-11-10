9568336469819dccc53cd33d64c60a2d658c9fb2
Kristjan Kosic <kristjan@protokol.com>
2020-11-10 14:56:56 +0100

    fix: process name pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	apps: [
 		{
-			name: "serve-data-prod",
+			name: "bucket-server",
 			script: "dist/main.js",
 
 			// Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
