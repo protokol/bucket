4dd09d4e06d65392dfe80175cc139d1c88749f56
Kristjan Kosic <kristjan@protokol.com>
2020-11-10 19:30:41 +0100

    feat: activating global validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe());
 
 	const options = new DocumentBuilder()
 		.setTitle("Protokol Bucket")
@@ -14,5 +16,6 @@ async function bootstrap() {
 	SwaggerModule.setup("docs", app, document);
 
 	await app.listen(40000);
+	console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
