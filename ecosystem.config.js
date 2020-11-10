4f848ca7ba7ef9548f3099e699c759f103f41db5
Kristjan Kosic <kristjan@protokol.com>
2020-11-10 14:55:09 +0100

    feat: add pm2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	apps: [
+		{
+			name: "serve-data-prod",
+			script: "dist/main.js",
+
+			// Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: "1G",
+			env: {
+				NODE_ENV: "production",
+			},
+		},
+	],
+};
